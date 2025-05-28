76a045630db82b154f8ec726abf6b214c9a05e20
John Jessop <jj576@cam.ac.uk>
2025-05-28 17:02:08 +0100

    Moved adders to DSP blocks

--- hardware/rtl/processor/verilog/alu.v
+++ hardware/rtl/processor/verilog/alu.v
@@ -1,52 +1,7 @@
-/*
-	Authored 2018-2019, Ryan Voo.
-
-	All rights reserved.
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions
-	are met:
-
-	*	Redistributions of source code must retain the above
-		copyright notice, this list of conditions and the following
-		disclaimer.
-
-	*	Redistributions in binary form must reproduce the above
-		copyright notice, this list of conditions and the following
-		disclaimer in the documentation and/or other materials
-		provided with the distribution.
-
-	*	Neither the name of the author nor the names of its
-		contributors may be used to endorse or promote products
-		derived from this software without specific prior written
-		permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-	FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-	COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-	INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-	BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-	LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-	ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	POSSIBILITY OF SUCH DAMAGE.
-*/
-
-
-
+`default_nettype none
+`timescale 1ns / 1ps
 `include "../include/rv32i-defines.v"
-`include "../include/sail-core-defines.v"
-
-
-
-/*
- *	Description:
- *
- *		This module implements the ALU for the RV32I.
- */
-
+`include "./subtractor.v"
 
 
 /*
@@ -60,6 +15,21 @@ module alu(ALUctl, A, B, ALUOut, Branch_Enable);
 	input [31:0]		B;
 	output reg [31:0]	ALUOut;
 	output reg		Branch_Enable;
+
+	wire[31:0] alu_addition;
+	wire[31:0] alu_subtraction;
+
+	adder alu_adder(
+			.input1(A),
+			.input2(B),
+			.out(alu_addition)
+		);
+
+	subtractor alu_subtractor(
+			.input1(A),
+			.input2(B),
+			.out(alu_subtraction)
+		);
 
 	/*
 	 *	This uses Yosys's support for nonzero initial values:
@@ -90,12 +60,12 @@ module alu(ALUctl, A, B, ALUOut, Branch_Enable);
 			/*
 			 *	ADD (the fields also match AUIPC, all loads, all stores, and ADDI)
 			 */
-			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_ADD:	ALUOut = A + B;
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_ADD:	ALUOut = alu_addition;
 
 			/*
 			 *	SUBTRACT (the fields also matches all branches)
 			 */
-			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SUB:	ALUOut = A - B;
+			`kSAIL_MICROARCHITECTURE_ALUCTL_3to0_SUB:	ALUOut = alu_subtraction;
 
 			/*
 			 *	SLT (the fields also matches all the other SLT variants)
--- hardware/rtl/processor/verilog/subtractor.v
+++ hardware/rtl/processor/verilog/subtractor.v
@@ -2,21 +2,23 @@
 `timescale 1ns / 1ps
 `include "../include/rv32i-defines.v"
 
-module adder (
+module subtractor (
     input [31:0] input1, input2,
     output [31:0] out
 );
 	wire CO_internal;
 
     SB_MAC16 i_sbmac16 (
-        .A(input1[31:16]),	// Assign adder inputs to 4 16 bit inputs
+        .A(input1[31:16]),	// Assign subtractor inputs to 4 16 bit inputs
         .B(input1[15:0]),
         .C(input2[31:16]),	
         .D(input2[15:0]),
-        .O(out),	
+        .O(out),
         .CE(1'b1),          // Always enable
         .CI(1'b0),          // No external carry-in
-        .CO(CO_internal)    // Capture carry-out from bottom
+        .CO(CO_internal),    // Capture carry-out from bottom
+        .ADDSUBBOT(1'b1),
+        .ADDSUBTOP(1'b1)
     );
 
     defparam i_sbmac16.TOPOUTPUT_SELECT = 2'b01;
@@ -25,5 +27,5 @@ module adder (
     defparam i_sbmac16.BOTOUTPUT_SELECT = 2'b01;
     defparam i_sbmac16.BOTADDSUB_UPPERINPUT = 1'b1;
     defparam i_sbmac16.BOTADDSUB_CARRYSELECT = 2'b00;
-
+    
 endmodule
