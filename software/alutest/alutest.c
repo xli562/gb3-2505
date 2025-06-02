2ab1ec3d2e02394248f93d7d41cab7731bd98200
Xiaoyang (Henry) Li <xl562@cam.ac.uk>
2025-06-02 23:09:03 +0100

    Change alutest to asm coding

--- software/alutest/alutest.c
+++ software/alutest/alutest.c
@@ -4,15 +4,6 @@
 
 #include "e-types.h"
 
-int a = 0;
-int b = 3;
-int c = 2;
-
 int main(void) {
-  volatile unsigned int *gDebugLedsMemoryMappedRegister = (unsigned int *)0x2000;
-  *gDebugLedsMemoryMappedRegister = 0b00000010;
-  a = b + c;
-  c = a + b;
-  *gDebugLedsMemoryMappedRegister = 0b00000001;
   return 0;
 }
