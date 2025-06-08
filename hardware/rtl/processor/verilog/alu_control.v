6b7fd0e021b039a6be6f564ba0b8c0fbcf09dd29
Xiaoyang (Henry) Li <xl562@cam.ac.uk>
2025-06-08 16:12:44 +0100

    Passed bsort

--- hardware/rtl/processor/verilog/alu_control.v
+++ hardware/rtl/processor/verilog/alu_control.v
@@ -10,93 +10,96 @@ module alu_control (
     output reg  [`kALU_OP_SEL_WIDTH-1:0] alu_op_sel_o,
     output reg  [`kALU_BRANCH_SEL_WIDTH-1:0] alu_branch_sel_o
 );
-    reg [6:0] ALUCtl;
     always @(*) begin
+        // Initialise values to avoid latches
+        alu_op_sel_o     = `kSAIL_ALUCTL_ILLEGAL;
+        alu_branch_sel_o = 0;
         if (~reset_n_i) begin
-            ALUCtl = 0;
+            alu_op_sel_o     = 0;
+            alu_branch_sel_o = 0;
         end else begin
             case (opcode_i)
                 `kRV32I_INSTRUCTION_OPCODE_LUI:
-                    ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_LUI;
+                    alu_op_sel_o = `kSAIL_ALUCTL_AND;
                 `kRV32I_INSTRUCTION_OPCODE_AUIPC:
-                    ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_AUIPC;
+                    alu_op_sel_o = `kSAIL_ALUCTL_ADD;
                 `kRV32I_INSTRUCTION_OPCODE_JAL:
-                    ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ILLEGAL;
+                    alu_op_sel_o = `kSAIL_ALUCTL_ILLEGAL;
                 `kRV32I_INSTRUCTION_OPCODE_JALR:
-                    ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ILLEGAL;
+                    alu_op_sel_o = `kSAIL_ALUCTL_ILLEGAL;
                 `kRV32I_INSTRUCTION_OPCODE_BRANCH:
                     case (funct3_i)
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_BEQ:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_BEQ;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_BNE:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_BNE;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_BLT:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_BLT;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_BGE:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_BGE;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_BLTU:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_BLTU;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_BGEU:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_BGEU;
+                        `kRV32I_FUNCT3_2to0_BEQ:
+                            alu_branch_sel_o = `kSAIL_ALUCTL_BEQ;
+                        `kRV32I_FUNCT3_2to0_BNE:
+                            alu_branch_sel_o = `kSAIL_ALUCTL_BNE;
+                        `kRV32I_FUNCT3_2to0_BLT:
+                            alu_branch_sel_o = `kSAIL_ALUCTL_BLT;
+                        `kRV32I_FUNCT3_2to0_BGE:
+                            alu_branch_sel_o = `kSAIL_ALUCTL_BGE;
+                        `kRV32I_FUNCT3_2to0_BLTU:
+                            alu_branch_sel_o = `kSAIL_ALUCTL_BLTU;
+                        `kRV32I_FUNCT3_2to0_BGEU:
+                            alu_branch_sel_o = `kSAIL_ALUCTL_BGEU;
                         default:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ILLEGAL;
+                            alu_op_sel_o = `kSAIL_ALUCTL_ILLEGAL;
                     endcase
 
                 `kRV32I_INSTRUCTION_OPCODE_LOAD:
                     case (funct3_i)
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_LB:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_LB;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_LH:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_LH;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_LW:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_LW;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_LBU:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_LBU;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_LHU:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_LHU;
+                        `kRV32I_FUNCT3_2to0_LB:
+                            alu_op_sel_o = `kSAIL_ALUCTL_ADD;
+                        `kRV32I_FUNCT3_2to0_LH:
+                            alu_op_sel_o = `kSAIL_ALUCTL_ADD;
+                        `kRV32I_FUNCT3_2to0_LW:
+                            alu_op_sel_o = `kSAIL_ALUCTL_ADD;
+                        `kRV32I_FUNCT3_2to0_LBU:
+                            alu_op_sel_o = `kSAIL_ALUCTL_ADD;
+                        `kRV32I_FUNCT3_2to0_LHU:
+                            alu_op_sel_o = `kSAIL_ALUCTL_ADD;
                         default:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ILLEGAL;
+                            alu_op_sel_o = `kSAIL_ALUCTL_ILLEGAL;
                     endcase
 
                 `kRV32I_INSTRUCTION_OPCODE_STORE:
                     case (funct3_i)
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_SB:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_SB;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_SH:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_SH;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_SW:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_SW;
+                        `kRV32I_FUNCT3_2to0_SB:
+                            alu_op_sel_o = `kSAIL_ALUCTL_ADD;
+                        `kRV32I_FUNCT3_2to0_SH:
+                            alu_op_sel_o = `kSAIL_ALUCTL_ADD;
+                        `kRV32I_FUNCT3_2to0_SW:
+                            alu_op_sel_o = `kSAIL_ALUCTL_ADD;
                         default:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ILLEGAL;
+                            alu_op_sel_o = `kSAIL_ALUCTL_ILLEGAL;
                     endcase
 
                 `kRV32I_INSTRUCTION_OPCODE_IMMOP:
                     case (funct3_i)
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_ADDI:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ADDI;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_SLTI:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_SLTI;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_SLTIU:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_SLTIU;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_XORI:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_XORI;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_ORI:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ORI;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_ANDI:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ANDI;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_SLLI:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_SLLI;
+                        `kRV32I_FUNCT3_2to0_ADDI:
+                            alu_op_sel_o = `kSAIL_ALUCTL_ADD;
+                        `kRV32I_FUNCT3_2to0_SLTI:
+                            alu_op_sel_o = `kSAIL_ALUCTL_SLT;
+                        `kRV32I_FUNCT3_2to0_SLTIU:
+                            alu_op_sel_o = `kSAIL_ALUCTL_SLT;
+                        `kRV32I_FUNCT3_2to0_XORI:
+                            alu_op_sel_o = `kSAIL_ALUCTL_XOR;
+                        `kRV32I_FUNCT3_2to0_ORI:
+                            alu_op_sel_o = `kSAIL_ALUCTL_OR;
+                        `kRV32I_FUNCT3_2to0_ANDI:
+                            alu_op_sel_o = `kSAIL_ALUCTL_AND;
+                        `kRV32I_FUNCT3_2to0_SLLI:
+                            alu_op_sel_o = `kSAIL_ALUCTL_SLL;
                         3'b101:
                             case (funct7_bit5_i)
                                 1'b0:
-                                    ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_SRLI;
+                                    alu_op_sel_o = `kSAIL_ALUCTL_SRL;
                                 1'b1:
-                                    ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_SRAI;
+                                    alu_op_sel_o = `kSAIL_ALUCTL_SRA;
                                 default:
-                                    ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ILLEGAL;
+                                    alu_op_sel_o = `kSAIL_ALUCTL_ILLEGAL;
                             endcase
                         default:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ILLEGAL;
+                            alu_op_sel_o = `kSAIL_ALUCTL_ILLEGAL;
                     endcase
 
                 `kRV32I_INSTRUCTION_OPCODE_ALUOP:
@@ -104,41 +107,40 @@ module alu_control (
                         3'b000:
                             case(funct7_bit5_i)
                                 1'b0:
-                                    ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ADD;
+                                    alu_op_sel_o = `kSAIL_ALUCTL_ADD;
                                 1'b1:
-                                    ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_SUB;
+                                    alu_op_sel_o = `kSAIL_ALUCTL_SUB;
                                 default:
-                                    ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ILLEGAL;
+                                    alu_op_sel_o = `kSAIL_ALUCTL_ILLEGAL;
                             endcase
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_SLL:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_SLL;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_SLT:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_SLT;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_SLTU:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_SLTU;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_XOR:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_XOR;
+                        `kRV32I_FUNCT3_2to0_SLL:
+                            alu_op_sel_o = `kSAIL_ALUCTL_SLL;
+                        `kRV32I_FUNCT3_2to0_SLT:
+                            alu_op_sel_o = `kSAIL_ALUCTL_SLT;
+                        `kRV32I_FUNCT3_2to0_SLTU:
+                            alu_op_sel_o = `kSAIL_ALUCTL_SLT;
+                        `kRV32I_FUNCT3_2to0_XOR:
+                            alu_op_sel_o = `kSAIL_ALUCTL_XOR;
                         3'b101:
                             case(funct7_bit5_i)
                                 1'b0:
-                                    ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_SRL;
+                                    alu_op_sel_o = `kSAIL_ALUCTL_SRL;
                                 1'b1:
-                                    ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_SRA; //SRA untested
+                                    alu_op_sel_o = `kSAIL_ALUCTL_SRA; //SRA untested
                                 default:
-                                    ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ILLEGAL;
+                                    alu_op_sel_o = `kSAIL_ALUCTL_ILLEGAL;
                             endcase
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_OR:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_OR;
-                        `kRV32I_INSTRUCTION_FUNCCODE_2to0_AND:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_AND;
+                        `kRV32I_FUNCT3_2to0_OR:
+                            alu_op_sel_o = `kSAIL_ALUCTL_OR;
+                        `kRV32I_FUNCT3_2to0_AND:
+                            alu_op_sel_o = `kSAIL_ALUCTL_AND;
                         default:
-                            ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ILLEGAL;
+                            alu_op_sel_o = `kSAIL_ALUCTL_ILLEGAL;
                     endcase
 
                 default:
-                    ALUCtl = `kSAIL_MICROARCHITECTURE_ALUCTL_6to0_ILLEGAL;
+                    alu_op_sel_o = `kSAIL_ALUCTL_ILLEGAL;
             endcase
         end
     end
-    assign {alu_branch_sel_o, alu_op_sel_o} = ALUCtl;
 endmodule
